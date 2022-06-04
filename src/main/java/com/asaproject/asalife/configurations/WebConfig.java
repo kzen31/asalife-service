0ca3eed973e1b765bb0954be862d7e7172ae2d9f
Muhammad Khuzain <mkhuzain17@gmail.com>
2022-06-04 14:14:31 +0800

    add cord for DELETE

--- src/main/java/com/asaproject/asalife/configurations/WebConfig.java
+++ src/main/java/com/asaproject/asalife/configurations/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("GET", "PUT", "POST");
+        registry.addMapping("/**").allowedMethods("GET", "PUT", "POST", "DELETE");
     }
     //allow spesific origin
 //    public void addCorsMappings(CorsRegistry registry) {
